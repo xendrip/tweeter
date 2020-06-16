271f3c6b87ccd7ec5602980e81f1787c3301a07b
Rubin <rjhand@gmail.com>
2020-06-16 23:34:33 +0000

    Add char count function: jQuery;

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  console.log("test test");
+  //stackoverflow 'how to get textarea text jquery'
+  const textArea = $('#tweet-text');
+  textArea.on('keyup', function() {
+    const charCount = $('.counter');
+    //stackoverflow 'jquery textarea character count'
+    const tweetLength = $(this).val().length;
+    //w3schools 'jquery changing color css'
+    let counter = 140;
+    if (counter - tweetLength < 0) {
+      charCount.css('color', 'red');
+    } else {
+      charCount.css('color', '');
+    }
+    charCount.text(counter - tweetLength);
+  });
 });
