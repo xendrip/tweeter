d3af9117b15ee80477176b6d9a143212be19d90e
Rubin <rjhand@gmail.com>
2020-06-17 23:38:38 +0000

    Add render function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,21 +3,33 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function() {
+
 
   //temp data; will eventually get from express server via AJAX GET request
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  };
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
 /* 
   <article class="tweet">
@@ -41,6 +53,13 @@ $(document).ready(function() {
     </footer>
   </article>
 */
+
+  const renderTweets = (tweets) => {
+    for (let tweet of tweets) {
+      const $tweetContent = createTweetElement(tweet);
+      $('#tweet-container').prepend($tweetContent); 
+    }
+  };
 
   //take in tweet object, return tweet <article> element containing entire HTML structure
   const createTweetElement = (tweetData) => {
@@ -76,8 +95,12 @@ $(document).ready(function() {
 
   };
   
-const $tweet = createTweetElement(tweetData);
+// const $tweet = createTweetElement(tweetData);
 
-console.log($tweet);
-$('#tweet-container').append($tweet);
+// console.log($tweet);
+// $('#tweet-container').append($tweet);
+
+$(document).ready(function() {
+  renderTweets(data);
+
 });
