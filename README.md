804324bbb303aa7a55104357c22d008d902ddd3f
Rubin <rjhand@gmail.com>
2020-06-22 02:39:18 +0000

    Add responsive design; clean up code

--- README.md
+++ README.md
@@ -2,16 +2,38 @@
 
 Tweeter is a simple, single-page Twitter clone.
 
-This repository is the starter code for the project: Students will fork and clone this repository, then build upon it to practice their HTML, CSS, JS, jQuery and AJAX front-end skills, and their Node, Express and MongoDB back-end skills.
+This app was built to practice `HTML`, `CSS`, `JS`, `jQuery` and `AJAX` front-end skills, and their `Node` and `Express` back-end skills.
 
-## Getting Started
+This app has a `responsive` layout that supports **Phone**, **Tablet**, and **Desktop** devices
 
-1. Fork this repository, then clone your fork of this repository.
-2. Install dependencies using the `npm install` command.
-3. Start the web server using the `npm run local` command. The app will be served at <http://localhost:8080/>.
-4. Go to <http://localhost:8080/> in your browser.
+# Final Product
+
+## Responsive Design
+
+### Desktop Layout
+!["Desktop Layout"](https://github.com/xendrip/tweeter/blob/master/docs/tweeter-1024px.png)
+
+### Tablet Layout
+!["Tablet Layout"](https://github.com/xendrip/tweeter/blob/master/docs/tweeter-768px.png)
+
+### Phone Layout: Landscape
+!["Tablet Layout"](https://github.com/xendrip/tweeter/blob/master/docs/tweeter-420px.png)
+
+### Phone Layout: Portrait
+!["Tablet Layout"](https://github.com/xendrip/tweeter/blob/master/docs/tweeter-lessthan420px.png)
+
+## General Features
+
+1) Users can submit tweets
+2) Users can browse all submitted tweets
+3) Character Counter
 
 ## Dependencies
 
 - Express
 - Node 5.10.x or above
+- Body Parser: 1.15.2
+- Chance: 1.0.2
+- Express": 4.13.4
+- md5: 2.1.0
+- Nodemon: 1.9.2
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,14 +5,18 @@
  */
 
 const validateTweet = (text) => {
+  const errorMessage = $('.error');
   if (!text.length) {
-    alert('Empty tweet, Try again');
+    errorMessage.slideDown(500);
+    errorMessage.text('Empty tweet, Try again');
     return false;
   }
   if (text.length > 140) {
-    alert('Your tweet is too long');
+    errorMessage.slideDown(500);
+    errorMessage.text('Your tweet is too long');
     return false;
   }
+  errorMessage.slideUp(500);
   return true;
 };
 
@@ -29,25 +33,32 @@ const createTweetElement = (tweetData) => {
   const { text } = tweetData.content;
   const createdTime = tweetData.created_at;
 
+  //not my code
+  const escape =  (str) => {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
   return `<article class="tweet">
-  <header>
-    <div class="userInfo">
-      <img class="avatar" src=${avatars}>
-      <span>${name}</span>
+    <header>
+      <div class="userInfo">
+        <img class="avatar" src=${avatars}>
+        <span>${name}</span>
+      </div>
+      <small class="user-handle hide">${handle}</small>
+    </header>
+    <div class="tweet-text">
+      <p>${escape(text)}</p>
     </div>
-    <small class="user-handle hide">${handle}</small>
-  </header>
-  <div class="tweet-text">
-    <p>${text}</p>
-  </div>
-  <footer>
-    <small>${createdTime}</small>
-    <span class="reaction">
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="fas fa-heart"></i>
-    </span>
-  </footer>
+    <footer>
+      <small>${Math.round((Date.now() - new Date(createdTime)) / (1000 * 60 * 60 * 24))} days ago</small>
+      <span class="reaction">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </span>
+    </footer>
   </article>`;
 
 };
